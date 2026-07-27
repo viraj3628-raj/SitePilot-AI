bdf39c7bdcd9baae6308975c5ef63b8e0a8e1f32
viraj3628-raj <Viraj3628@gmail.com>
2026-07-27 23:01:57 +0530

    Fix Gemini model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
     const { message } = await req.json();
 
     const response = await ai.models.generateContent({
-   model: "gemini-2.5-flash",
+   model: "gemini-2.5-flash-lite",
       contents: message,
     });
 
